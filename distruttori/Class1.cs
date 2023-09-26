bbac5a016487c7ee781f0037654c1b772187fd00
Yan Nascimento <yanzebedeu@hotmail.com>
2023-09-26 12:28:16 +0200

    Aggiungere i file di progetto.

---
+++ distruttori/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace distruttori
+{
+    public class Class1
+    {
+        private string nome;
+        private int numero;
+        public Class1()
+        {
+            nome = "";
+            numero = 0;
+        }
+        public Class1(string n,int num)
+        {
+            setnome(n);
+            setnum(num);
+        }
+        public string getnome()
+        {
+            return nome;
+        }
+        public double getnum()
+        {
+            return numero;
+        }
+        public void setnum(int num)
+        {
+            if (num > 0)
+            {
+                numero = num;
+            }
+        }
+        public void setnome(string n)
+        {
+            nome = n;
+        }
+        ~Class1()
+        {
+            Console.WriteLine("Distruttore in chiamata");
+        }
+    }
+}
